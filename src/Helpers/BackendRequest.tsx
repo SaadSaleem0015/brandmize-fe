10200db1caaa8b34cbf1318a4cab6470a1e8d27d
Usmanghani1518 <mrusmanghani8@gmail.com>
2026-04-15 22:26:40 +0500

    fix:add update the socket url

--- src/Helpers/BackendRequest.tsx
+++ src/Helpers/BackendRequest.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://localhost:8000/api/v1",
     
-  baseURL: "/api/v1",
+  // baseURL: "/api/v1",
   
 
   withCredentials: true, // Required for HttpOnly cookies
